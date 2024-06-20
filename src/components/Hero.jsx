07d82ec61a442347095f2fc3e912cf909fe75639
Ark2044 <crce.9517.ecs@gmail.com>
2024-06-20 20:49:11 +0530

    reduced number of stars

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,11 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
-import HeroBg from "../assets/herobg.png";
-
 const Hero = () => {
   return (
-    <section
-      className={`relative w-full h-screen mx-auto bg-cover bg-center bg-no-repeat`}
-      style={{
-        backgroundImage: `url(${HeroBg})`,
-      }}
-    >
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
